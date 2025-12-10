68f86547297eea92ba5f48dab58a08d87633d229
Doww <kay.ngoquoctinh@gmail.com>
2025-12-11 01:41:33 +0700

    add model cls

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ def main():
     globals.give_way_shared = shared_give_way
     
     threading.Thread(target=play_sound, args=("start-program.mp3",)).start()
-    # threading.Thread(target=save_regisstered_vehicles_to_file, args=(get_registered_vehicles(),)).start()
-    # threading.Thread(target=tracking_car.start_tracking_car, daemon=True).start()
-    # threading.Thread(target=detect_license.start_detect_license, daemon=True).start()
+    threading.Thread(target=save_regisstered_vehicles_to_file, args=(get_registered_vehicles(),)).start()
+    threading.Thread(target=tracking_car.start_tracking_car, daemon=True).start()
+    threading.Thread(target=detect_license.start_detect_license, daemon=True).start()
     threading.Thread(target=connect_bgm220.start_connect_bgm220, daemon=True).start()
-    # threading.Thread(target=connect_xg26.start_connect_xg26, daemon=True).start()
-    threading.Thread(target=turn_light_barier.start_turn_light_servo, daemon=True).start()
+    threading.Thread(target=connect_xg26.start_connect_xg26, daemon=True).start()
+    threading.Thread(target=turn_light_barier.start_turn_light_barier, daemon=True).start()
 if __name__ == '__main__':
     main()
     while True:
